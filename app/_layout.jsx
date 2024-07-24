da59b3ff3b565f31b52e01fbac8612c9ca88a558
tatsuoNakano <aaaoauo1118@gmeil.com>
2024-07-25 08:50:18 +0900

    vector icon設定　fontAwesome使用

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,10 +1,46 @@
 import { Tabs } from 'expo-router';
-import { Ionicons } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+
 
 export default function Layout() {
     return (
         <Tabs>
-
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: 'Pomodoro',
+                    tabBarIcon: ({ color, size }) => (
+                        <FontAwesome name="bell" size={24} color="black" />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="graph"
+                options={{
+                    title: 'Graph',
+                    tabBarIcon: ({ color, size }) => (
+                        <FontAwesome name="bar-chart-o" size={24} color="black" />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="information"
+                options={{
+                    title: 'Information',
+                    tabBarIcon: ({ color, size }) => (
+                        <FontAwesome name="info" size={24} color="black" />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="setting"
+                options={{
+                    title: 'Setting',
+                    tabBarIcon: ({ color, size }) => (
+                        <FontAwesome name="gear" size={24} color="black" />
+                    ),
+                }}
+            />
         </Tabs>
     );
 }
