e1e4206c14c4827cf5a3de13374d1e9bd5ad7d80
tatsuoNakano <aaaoauo1118@gmeil.com>
2024-07-25 05:38:35 +0900

    screenの各それぞれの画面に最小構成画面を作成

---
+++ src/screen/information.jsx
@@ -0,0 +1,17 @@
+
+
+// 最初にReactとReact Nativeをインポートします
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// MyApp()という関数を作成します。これはReactのコンポーネントで、画面に何を要表示するかをReactに伝えます
+function Infornation() {
+    return (
+        <View>
+            <Text>Hello, World!</Text>
+        </View>
+    );
+}
+
+// MyAppコンポーネントをエクスポートします。これにより他のファイルからインポートして使用することができます
+export default Infornation;
